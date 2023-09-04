51cc1cd5c263d8a55042090fd0395a193de477e0
momajdou <momajdou@e1r8p1.1337.ma>
2023-09-04 22:40:06 +0100

    last commit

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: momajdou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/23 20:08:06 by momajdou          #+#    #+#             */
-/*   Updated: 2023/08/26 14:35:47 by momajdou         ###   ########.fr       */
+/*   Created: 2023/09/04 21:57:33 by momajdou          #+#    #+#             */
+/*   Updated: 2023/09/04 21:57:37 by momajdou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	unsigned int	i;
+	int	i;
 
 	i = 0;
 	while (str[i])
@@ -24,25 +24,21 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	src_len;
-	unsigned int	dest_len;
+	unsigned int	len_src;
+	unsigned int	len_dest;
 
+	len_src = ft_strlen(src);
+	len_dest = ft_strlen(dest);
+	j = len_dest;
 	i = 0;
-	j = 0;
-	src_len = ft_strlen(src);
-	dest_len = ft_strlen(dest);
-	while (dest[i] != '\0')
-		i++;
-	if (size == 0 || i >= size)
-		return (src_len + size);
-	if ((src_len + dest_len) >= size)
-		return (src_len + dest_len);
-	while (src[j] && i < size - 1)
+	if (size == 0 || size <= len_dest)
+		return (len_src + size);
+	while (src[i] && i < size - len_dest - 1)
 	{
-		dest[i] = src[j];
+		dest[j] = src[i];
 		j++;
 		i++;
 	}
-	dest[i] = '\0';
-	return (i);
+	dest[j] = '\0';
+	return (len_src + len_dest);
 }
